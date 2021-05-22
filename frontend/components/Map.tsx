c583251c96108f5111cde2a293fc834692c594a1
gabrielnsd <gabrielnsdantas@gmail.com>
2021-05-22 08:05:36 -0300

    map modal created

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -3,13 +3,21 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 
-const Map = () => {
+
+interface MapProps {
+  initialLocation: Array<any>
+}
+
+const Map = (props: MapProps) => {
+  let initialLocation: [number, number]
+  initialLocation = [parseFloat(props.initialLocation[0]), parseFloat(props.initialLocation[1])]
+
   return (
     <MapContainer
-      center={[-5.9231, -35.2677]}
+      center={initialLocation}
       zoom={14}
       scrollWheelZoom={true}
-      style={{ height: "50vh", width: "90vw", margin: "0 auto" }}
+      style={{ height: "100%", width: "100%", margin: "0 auto" }}
     >
       <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ2FicmllbG5zZCIsImEiOiJja296NGxyeDIwZnNzMnB0YWJjZjY4N282In0.R6cP6EYyGDCHBWfdpsRkhw" />
       <Marker position={[-5.9231, -35.2677]} draggable={true} >
