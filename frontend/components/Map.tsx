db6aef3cab5da499b9b9312f70015b3fe5eab1ef
gabrielnsd <gabrielnsdantas@gmail.com>
2021-05-22 00:06:46 -0300

    leaflet map added
    Co-authored-by: Manoel <ManoelLobo@users.noreply.github.com>

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -1,22 +1,26 @@
-import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 
 const Map = () => {
   return (
-    <MapContainer center={[40.8054,-74.0241]} zoom={14} scrollWheelZoom={false} style={{height: "100%", width: "100%"}}>
-      <Marker 
-      position={[40.8054,-74.0241]}
-      draggable={true}
-      //animate={true}
-      >
-        <Popup>
-          Hey ! you found me
-        </Popup>
+    <MapContainer
+      center={[-5.9231, -35.2677]}
+      zoom={14}
+      scrollWheelZoom={true}
+      style={{ height: "50vh", width: "50vw", margin: "0 auto" }}
+    >
+      <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ2FicmllbG5zZCIsImEiOiJja296NGxyeDIwZnNzMnB0YWJjZjY4N282In0.R6cP6EYyGDCHBWfdpsRkhw" />
+      <Marker position={[-5.9231, -35.2677]} draggable={true} >
+        <Popup>Hey ! I live here</Popup>
+      </Marker>
+      <Marker position={[-5.9000, -35.2677]} draggable={true} >
+        <Popup>Hey ! I live here</Popup>
       </Marker>
     </MapContainer>
-  )
-}
+  );
+};
 
-export default Map
+
+export default Map;
