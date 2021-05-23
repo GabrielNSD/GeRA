d7075da5c8b90762d1f1985153367d717f693d7c
Manoel <manoel.lobo@gmail.com>
2021-05-23 00:45:45 -0300

    optional draggable center marker

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -1,28 +1,34 @@
-import { MapContainer, TileLayer, Marker, Popup, Polygon } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import "leaflet-defaulticon-compatibility"
 
 interface MapProps {
-  initialLocation: Array<any>;
-  polygons?: Array<any>;
+  initialLocation: Array<any>
+  polygons?: Array<any>
+  markers?: Array<any>
+  centerMarker?: boolean
 }
 
 const Map = (props: MapProps) => {
-  let initialLocation: [number, number];
+  let initialLocation: [number, number]
   initialLocation = [
     parseFloat(props.initialLocation[0]),
     parseFloat(props.initialLocation[1]),
-  ];
+  ]
 
-  const testArea = props.polygons ? (props.polygons.length ?  props.polygons[0] : null) : null;
+  const testArea = props.polygons
+    ? props.polygons.length
+      ? props.polygons[0]
+      : null
+    : null
 
-  let polygons = [];
+  let polygons = []
   if (props.polygons && props.polygons.length > 0) {
     polygons = props.polygons //.map((item: any) => item[1]);
   }
 
-  console.log("testArea ", testArea);
+  console.log("testArea ", testArea)
   //console.log("polygons ", polygons);
 
   return (
@@ -34,12 +40,19 @@ const Map = (props: MapProps) => {
       //bounds={}
     >
       {polygons
-        ? polygons.map((item: any) => <Polygon key={item[0]} positions={item[1]}></Polygon>)
+        ? polygons.map((item: any) => (
+            <Polygon key={item[0]} positions={item[1]}></Polygon>
+          ))
         : null}
+
+      {props.centerMarker && (
+        <Marker position={initialLocation} draggable={true} />
+      )}
+
       {/* <Polygon positions={testArea}></Polygon> */}
       <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ2FicmllbG5zZCIsImEiOiJja296NGxyeDIwZnNzMnB0YWJjZjY4N282In0.R6cP6EYyGDCHBWfdpsRkhw" />
     </MapContainer>
-  );
-};
+  )
+}
 
-export default Map;
+export default Map
--- frontend/components/Modal/ModalMap.tsx
+++ frontend/components/Modal/ModalMap.tsx
@@ -1,27 +1,30 @@
-import Map from "../Map"
 import dynamic from "next/dynamic"
 
 interface ModalMapProps {
-    openModalMap: boolean
-    initialLocation: Array<string>
+  openModalMap: boolean
+  initialLocation: Array<string>
 }
 
 const ModalMap = (props: ModalMapProps) => {
-    const MapWithNoSSR = dynamic(() => import("../../components/Map"), {
-        ssr: false
-      });
-    let modalClasses =
-    "h-1/2 bg-white shadow-md fixed -bottom-full w-1/2 z-20  pl-4 transform translate-y-full transition-transform duration-300 ease-out";
+  const MapWithNoSSR = dynamic(() => import("../../components/Map"), {
+    ssr: false,
+  })
+
+  let modalClasses =
+    "h-1/2 bg-white shadow-md fixed -bottom-full w-1/2 z-20  pl-4 transform translate-y-full transition-transform duration-300 ease-out"
 
   if (props.openModalMap) {
     modalClasses =
-      "rounded-2xl h-1/2 bg-white shadow-md fixed left-1/3 top-1/3 w-1/2 z-20  p-2 transform translate-x-0 flex";
+      "rounded-2xl h-1/2 bg-white shadow-md fixed left-1/3 top-1/3 w-1/2 z-20  p-2 transform translate-x-0 flex"
   }
 
-  return(
-      <div className={modalClasses}>
-          <MapWithNoSSR initialLocation={props.initialLocation}/>
-      </div>
+  return (
+    <div className={modalClasses}>
+      <MapWithNoSSR
+        initialLocation={props.initialLocation}
+        centerMarker={true}
+      />
+    </div>
   )
 }
 
